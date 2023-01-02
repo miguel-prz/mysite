4b5fdce970f6c00da428abedbb64e72eee0efcf7
Miguel Amauri Pérez Rodríguez <aperez@zapateriasmexico.com.mx>
2023-01-02 15:40:35 -0600

    Agregaron funciones backend

---
+++ js/functions.php
@@ -0,0 +1,11 @@
+<?php 
+    define("USER",zapmex);
+    define("PSSW",zapmex);
+    define("DB",zapmex);
+    define("SERVER",zapmex);
+    define("PORT",zapmex);
+
+    function saltoCMD(){
+        echo "/n";
+    }
+?>
